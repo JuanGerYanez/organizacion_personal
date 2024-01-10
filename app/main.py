ad237920d101b7ef8475d6fe6ce4531e24e480e2
JuanGerYanez <97035874+JuanGerYanez@users.noreply.github.com>
2024-01-10 00:24:32 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,4 +9,4 @@ app.include_router(router_actividad_diaria, prefix="/api/v1")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="localhost", port=1000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
